020143b45169e22ccb3997aafe4d69984cdb7af5
Christoffer Dahlen <christoffer.dahlen@powel.se>
2021-03-03 15:11:22 +0100

    TEST

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -2,5 +2,12 @@
 {
     public class Class1
     {
+
+        public int nSTAT()
+        {
+            var sta = 2;
+            return "STTS";
+        }
+        
     }
 }
---
+++ Solution1/ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+        }
+
+        public void OnEventOccuring(Event occuredEvent)
+        {
+            PerformAction();
+            Console.WriteLine("ttta");
+        }
+    }
+
+    internal class Event
+    {
+    }
+}
